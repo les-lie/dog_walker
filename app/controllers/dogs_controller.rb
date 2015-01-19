1b93f40dee9356398b3d5646616c0e9ecdc8dbc3
Leslie <lesliemariebell@gmail.com>
2015-01-19 07:16:07 -0600

    Update photos

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -40,7 +40,7 @@ class DogsController < ApplicationController
 	private
 
 	def dog_params
-		params.require(:dog).permit(:name, :description)
+		params.require(:dog).permit(:name, :description, :image)
 	end
 	def find_dog
 		@dog = Dog.find(params[:id])
