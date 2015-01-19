7e0885c7cffdcdef1d9c8f3e0db8b05fc3d6fa5d
Leslie <lesliemariebell@gmail.com>
2015-01-19 09:50:06 -0600

    Niko

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,4 @@
 class Dog < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300" }
+  has_attached_file :image, :styles => { :medium => "300x300#" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
